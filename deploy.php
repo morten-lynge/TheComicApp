31ab6a912d9cabac8b65d0a2e62b4d2d27693777
Morten Lynge <mortenll@adslhome.dk>
2018-07-15 16:26:31 +0200

    Testing

--- deploy.php
+++ deploy.php
@@ -4,13 +4,13 @@ namespace Deployer;
 require 'recipe/laravel.php';
 
 // Project name
-set('application', 'my_project');
+set('application', 'TheComicApp');
 
 // Project repository
-set('repository', 'git@domain.com:username/repository.git');
+set('repository', 'git@github.com:morten-lynge/TheComicApp.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', false); 
 
 // Shared files/dirs between deploys 
 add('shared_files', []);
@@ -22,8 +22,10 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
+host('18.185.103.78')
+	->user('deployer')
+	->identityFile('~/.ssh/deployerkey')
+    ->set('deploy_path', '/var/www/html/TheComicApp');    
     
 // Tasks
 
@@ -31,10 +33,15 @@ task('build', function () {
     run('cd {{release_path}} && build');
 });
 
+task('pwd', function () {
+    $result = run('pwd');
+    writeln("Current dir: $result");
+});
+
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-before('deploy:symlink', 'artisan:migrate');
+// before('deploy:symlink', 'artisan:migrate');
 
